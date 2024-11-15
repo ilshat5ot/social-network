ff712736c459adacf86dff3c9d714b1b23cd0da4
ilshat <ilshat.sadykov@v6services.ru>
2024-11-15 13:31:57 +0300

    SN-01: refactor

--- friend-service/src/main/java/ru/sadykov/friendservice/friend/exception/InvalidRequestParameterException.java
+++ friend-service/src/main/java/ru/sadykov/friendservice/friend/exception/InvalidRequestParameterException.java
@@ -2,9 +2,6 @@ package ru.sadykov.friendservice.friend.exception;
 
 public class InvalidRequestParameterException extends RuntimeException {
 
-    public InvalidRequestParameterException() {
-    }
-
     public InvalidRequestParameterException(String message) {
         super(message);
     }
--- friend-service/src/main/java/ru/sadykov/friendservice/friend/exception/UserNotFoundException.java
+++ friend-service/src/main/java/ru/sadykov/friendservice/friend/exception/UserNotFoundException.java
@@ -2,9 +2,6 @@ package ru.sadykov.friendservice.friend.exception;
 
 public class UserNotFoundException extends RuntimeException {
 
-    public UserNotFoundException() {
-    }
-
     public UserNotFoundException(String message) {
         super(message);
     }
